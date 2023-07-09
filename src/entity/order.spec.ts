d5cb2f8151d515fde434468523e8dab61cd8167a
JP-Cardoso <jhonpoal@hotmail.com>
2023-07-09 15:45:26 -0300

    feat: End or order test, and adjust in swc and jest

--- src/entity/order.spec.ts
+++ src/entity/order.spec.ts
@@ -1,13 +1,13 @@
 import Order from "./order";
 import OrderItem from "./order_item";
 
-describe('Order uinit tests', () => {  
+describe('Order uinit tests', () => {
 
     it('Should throw error when id is empty', () => {
         expect(() => {
             let order = new Order("", "123", []);
         }).toThrowError("ID is required")
-    });   
+    });
 
     it('Should throw error when customerId is empty', () => {
         expect(() => {
@@ -22,13 +22,22 @@ describe('Order uinit tests', () => {
     })
 
     it('Should calculate total', () => {
-        const item = new OrderItem("1", "item 1", 1000);
-        const item2 = new OrderItem("2", "item 2", 500);
-        const item3 = new OrderItem("3", "item 3", 1000);
-        
+        const item = new OrderItem("1", "item 1", 1000, 'p1', 2);
+        const item2 = new OrderItem("2", "item 2", 500, 'p2', 3);
+        const item3 = new OrderItem("3", "item 3", 1000, 'p3', 2);
+
         const order = new Order("O1", "C1", [item, item2, item3]);
         const total = order.total();
 
-        expect(total).toBe(2500)
+        expect(total).toBe(5500)
+    })
+
+    it('should throw error if the item qtd is less or equal zero', () => {
+        expect(() => {
+            const item = new OrderItem("1", "item 1", 1000, 'p1', 0);
+            const order = new Order("O1", "C1", [item]);
+
+        }).toThrowError('Quantity must be greater than zero');
+
     })
 });
--- src/entity/order.ts
+++ src/entity/order.ts
@@ -27,15 +27,18 @@ export default class Order {
         };
         if (this._customerId.length === 0) {
             throw new Error("CustomerId is required")
-        }
-        if(this._items.length === 0) {
+        };
+        if (this._items.length === 0) {
             throw new Error("Item qtd must be greater than 0");
+        };
+        if(this._items.some(item => item.quantity <= 0)) {
+            throw new Error("Quantity must be greater than zero");            
         }
         return true;
     }
 
     total(): number {
-        return this._items.reduce((acc, item) => acc + item._price, 0);
+        return this._items.reduce((acc, item) => acc + item.price, 0);
     }
-    
+
 }
