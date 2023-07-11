df3b53a3efd0048763645a493ac302da9876d485
JP-Cardoso <jhonpoal@hotmail.com>
2023-07-10 21:26:37 -0300

    feat: configuration for sequelize to run on jest

--- src/infrastructure/repository/product.repository.spec.ts
+++ src/infrastructure/repository/product.repository.spec.ts
@@ -1,4 +1,5 @@
 import { Sequelize } from "sequelize-typescript";
+import ProductModel from "../db/sequelize/model/product.model";
 
 describe('Product repository test', () => {
 
@@ -11,8 +12,11 @@ describe('Product repository test', () => {
             sync: { force: true }
         });
 
-        afterEach(async () => {
-            await sequelize.close();
-        });
-    })
+        //Adição do model, para dizer ao sequelize que esse model existe
+        sequelize.addModels([ProductModel]);
+        await sequelize.sync();     
+    });
+    afterEach(async () => {
+        await sequelize.close();
+    });
 })
